189db4cff799eae20d67433ee803a424d86f7713
admin <franpan_fp@hotmail.com>
2018-06-14 15:20:32 -0600

    lab theming

---
+++ web/modules/custom/itm_lab3/src/Plugin/Block/lab3FieldFormatter.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\itm_lab3\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+
+/**
+ * Provides a 'lab3FieldFormatter' block.
+ *
+ * @Block(
+ *  id = "lab3field_formatter",
+ *  admin_label = @Translation("Lab3field formatter"),
+ * )
+ */
+class lab3FieldFormatter extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Drupal\Core\Session\AccountProxyInterface definition.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+  /**
+   * Constructs a new lab3FieldFormatter object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param string $plugin_definition
+   *   The plugin implementation definition.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    AccountProxyInterface $current_user
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->currentUser = $current_user;
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_user')
+    );
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['lab3field_formatter']['#markup'] = format_date(\Drupal::time()->getRequestTime(), 'long');
+
+
+    return $build;
+  }
+
+}
---
+++ web/modules/custom/itm_lab3/src/Plugin/Field/FieldFormatter/lab3FieldFormatter.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\itm_lab3\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'lab3field_formatter' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "lab3field_formatter",
+ *   label = @Translation("Lab3field formatter"),
+ *   field_types = {
+ *     "timestamp"
+ *   }
+ * )
+ */
+class lab3FieldFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
+    foreach ($items as $delta => $item) {
+      $elements[$delta] = ['#markup' => $this->viewValue($item)];
+    }
+
+    return $elements;
+  }
+
+  /**
+   * Generate the output appropriate for one field item.
+   *
+   * @param \Drupal\Core\Field\FieldItemInterface $item
+   *   One field item.
+   *
+   * @return string
+   *   The textual output generated.
+   */
+  protected function viewValue(FieldItemInterface $item) {
+    // The text value has no text format assigned to it, so the user input
+    // should equal the output, including newlines.
+    $item = \Drupal::time()->getRequestTime();
+    return nl2br(Html::escape($item->value));
+  }
+
+}
